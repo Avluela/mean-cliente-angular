a30c6fc397410ca0807610808c628a1c62af5c8e
Gretel-Barrios <gretel.barrios@vanesa.com.ar>
2022-10-02 17:39:53 -0300

    actualizacion de clases

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CrearProductoComponent } from './components/crear-producto/crear-producto.component';
 
-const routes: Routes = [];
+//Components
+import { ListarProductosComponent } from './components/listar-productos/listar-productos.component';
+
+const routes: Routes = [
+  { path: '', component: ListarProductosComponent},
+  { path: 'crear-producto', component: CrearProductoComponent},
+  { path: 'editar-producto/:id', component: CrearProductoComponent},
+  { path: '**', redirectTo: '', pathMatch:'full'} //Redirecciona a la ruta principal en cualquier otra ruta que no sea la declaradas
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/components/crear-producto/crear-producto.component.ts
+++ src/app/components/crear-producto/crear-producto.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Producto } from 'src/app/models/producto';
 
 @Component({
   selector: 'app-crear-producto',
@@ -6,10 +9,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crear-producto.component.css']
 })
 export class CrearProductoComponent implements OnInit {
+  productoForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder,
+              private router: Router) {
+    this.productoForm = this.fb.group({
+      producto: ['', Validators.required],
+      categoria: ['', Validators.required],
+      ubicacion: ['', Validators.required],
+      precio: ['', Validators.required],
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  agregarProducto(){
+    // visualizar por consola todo el form
+    console.log(this.productoForm);
+
+    // acceder a un valor del form
+    console.log(this.productoForm.get('producto')?.value);
+
+    // Creo una constante Producto de tipo producto, para poder pasarle los datos del form al back
+    // el tipo producto se importa arriba linea 4
+    const PRODUCTO: Producto = {
+      name: this.productoForm.get('producto')?.value,
+      category: this.productoForm.get('categoria')?.value,
+      location: this.productoForm.get('ubicacion')?.value,
+      price: this.productoForm.get('precio')?.value,
+    }
+
+    console.log(PRODUCTO);
+    // Para poder usar esta funcion es necesario importar Router en linea 15 y 3
+    this.router.navigate(['/']);
   }
 
 }
